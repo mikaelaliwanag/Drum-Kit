aa04641bd691ecce4bd6046c4cd38d78eecd5f91
Grasshopper <mikaela301@gmail.com>
2020-05-02 21:08:25 -0700

    Added keydown event

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
+//For button press
+
 var drumButtons = document.querySelectorAll(".drum");
 
 for (var i = 0; i < drumButtons.length; i++) {
+	drumButtons[i].addEventListener("click", function () {
 
-   drumButtons[i].addEventListener("click", function () {
-	  
-	var buttonInnerHTML = this.innerHTML;
+		var buttonInnerHTML = this.innerHTML;
 
-	switch (buttonInnerHTML) {
-		case "w": 
-		  var tom1 = new Audio("sounds/tom-1.mp3");
-          tom1.play();
-		  break;
-		
-		case "a":
-		  var tom2 = new Audio("sounds/tom-2.mp3");
-          tom2.play();
-		  break;
+		playSound(buttonInnerHTML);
 
-		case "s": 
-		  var tom3 = new Audio("sounds/tom-3.mp3");
-          tom3.play();
-		  break;
-		
-		case "d": 
-		  var tom4 = new Audio("sounds/tom-4.mp3");
-          tom4.play();
-		  break;
-
-		case "j": 
-		  var crash = new Audio("sounds/crash.mp3");
-          crash.play();
-		  break;
-
-		case "k": 
-		  var snare = new Audio("sounds/snare.mp3");
-          snare.play();
-		  break;
-
-		case "l": 
-		  var kick = new Audio("sounds/kick-bass.mp3");
-          kick.play();
-		  break;
-
-		default: console.log(buttonInnerHTML);
-			break;
-	}
-
-});
+	});
 }
 
+//For keyboard press
 
+document.addEventListener("keydown", function (event) {
 
+	   playSound(event.key);
+
+});
+
+function playSound(key) {
+
+	switch (key) {
+		case "w":
+			var tom1 = new Audio("sounds/tom-1.mp3");
+			tom1.play();
+			break;
+
+		case "a":
+			var tom2 = new Audio("sounds/tom-2.mp3");
+			tom2.play();
+			break;
+
+		case "s":
+			var tom3 = new Audio("sounds/tom-3.mp3");
+			tom3.play();
+			break;
+
+		case "d":
+			var tom4 = new Audio("sounds/tom-4.mp3");
+			tom4.play();
+			break;
+
+		case "j":
+			var crash = new Audio("sounds/crash.mp3");
+			crash.play();
+			break;
+
+		case "k":
+			var snare = new Audio("sounds/snare.mp3");
+			snare.play();
+			break;
+
+		case "l":
+			var kick = new Audio("sounds/kick-bass.mp3");
+			kick.play();
+			break;
+
+		default:
+			console.log(buttonInnerHTML);
+			break;
+	}
+}
